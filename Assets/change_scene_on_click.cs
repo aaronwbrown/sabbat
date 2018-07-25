3529b102b98b0b5af6f735e3b0ec5cb304156bca
abrown <abrown@lendingclub.com>
2018-07-24 21:46:05 -0700

    setup version control for sabbat

---
+++ Assets/change_scene_on_click.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+
+public class change_scene_on_click : MonoBehaviour {
+
+  private void Start() {
+      GetComponent<Button>().onClick.AddListener(ChangeScene);
+  }
+
+  public void ChangeScene(){
+      SceneManager.LoadScene("mission_menu", LoadSceneMode.Single);
+ }
+
+}
