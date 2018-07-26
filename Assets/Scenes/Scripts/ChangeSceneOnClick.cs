27a39b8892de9aac9ba968ac7d9f6bc5567d9b75
abrown <abrown@lendingclub.com>
2018-07-25 19:11:29 -0700

    - mission menu added - mission prefab button created

---
+++ Assets/Scenes/Scripts/ChangeSceneOnClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnClick : MonoBehaviour {
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ChangeScene);
+    }
+
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("mission_menu", LoadSceneMode.Single);
+    }
+
+}
