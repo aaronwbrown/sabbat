3ba2f522195744586afe135ef122e30f8152243b
Mario Izquierdo <marioizquierdo@twitch.tv>
2018-11-01 23:00:12 -0700

    simplify

--- Assets/Scenes/GameBoardMain/GameStateTest.cs
+++ Assets/Scenes/GameBoardMain/GameStateTest.cs
@@ -7,21 +7,18 @@ public class GameStateTest : MonoBehaviour {
 
     // This script is used in a test scene to exercise the GameState utility object
     void Start () {
-        var game = new GameState();
+        // var game = new GameState();
 
-        test("Player has at least one card", () => {
+        test("One is equal to one", () => {
             if (1 != 1) {
-                throw new Exception("foobar");
+                throw new Exception("Expected 1 to be == 1");
             }
         });
     }
 
     void test(string title, Action testBlock) {
-        p("TEST: {0}", title);
+        Debug.LogFormat("TEST: {0}", title);
         testBlock();
-    }
-    void p(string format, params object[] args) {
-        Debug.LogFormat(format, args);
     }
 }
 
